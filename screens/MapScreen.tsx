36f84e49eab18ea5b66f21dc92531c19a7ac809b
sorasarah <ouriachi.sarah@gmail.com>
2023-01-06 16:27:01 +0100

    test

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -3,7 +3,7 @@ import { Platform, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-
+//
 export default function MapScreen(){
   
 
